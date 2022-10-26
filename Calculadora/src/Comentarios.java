fc7a8b1216a4333ad91760309e7a1cea14d306e5
Pallas <rod.palanca22@gmail.com>
2022-10-26 18:18:07 -0300

    finalizando exercicio

---
+++ Calculadora/src/Comentarios.java
@@ -0,0 +1,13 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // ola, eu sou um comentario em uma unica linha
+
+        /*
+        * Este método foi elaborado as pressas
+        * por isso eu abrevei o nome das variáveis
+        * mas olha, ele tem a finalidade de somar ou  multiplicar
+        * dois números
+        * /
+
+    }
+}
