8800e6133d4e45367448b29f796e9673d666baa1
Pallas <rod.palanca22@gmail.com>
2022-10-26 14:08:30 -0300

    termino do teste

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        
+
     }
 }
